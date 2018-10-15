451aaa21e3493ca1f94b52c9f587aba5b7e633a9
Amandine95 <yangguanglg@163.com>
2018-10-15 21:23:51 +0800

    导入数据库模型&测试数据

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ from flask import session, current_app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from info import create_app, db
+from info import create_app, db, models
 
 # 通过配置名创建app
 app = create_app('develop')
