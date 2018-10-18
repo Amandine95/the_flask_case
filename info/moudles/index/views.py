77c6e9bb587320e7a3dc0e64be49866080e16576
Amandine95 <yangguanglg@163.com>
2018-10-18 19:30:40 +0800

    登录状态显示、头像昵称的显示

--- info/moudles/index/views.py
+++ info/moudles/index/views.py
@@ -1,15 +1,34 @@
 """添加网页页签图片的路由，这个请求路径是固定的  /文件名 """
 from info import redis_store
+from info.models import User
 from . import index_blu
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 
 # 注册路由
 @index_blu.route('/')
 def index():
-    redis_store.set('sam_smith', 'billy')
-
-    return render_template('news/index.html')
+    """
+    返回主页(登陆后状态显示)
+    0、登录注册后session中都会存储用户数据
+    1、已登录、查询(session中获取)用户数据(nick_name,avatar)显示在模板
+    2、未登录，查询不到数据，显示默认
+    """
+    # 获取session数据
+    user_id = session.get('user_id', None)
+    user = None
+    if user_id:
+        try:
+            # 查询数据库
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    # user有值，执行user.to_dict()；如果user没有值，将None给"user"
+    data = {
+        "user": user.to_dict() if user else None
+    }
+    # 渲染模板
+    return render_template('news/index.html', data=data)
 
 
 # 图标路由
