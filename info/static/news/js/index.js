9978163c372c386a43856ddcd4dad4886c6c316a
Amandine95 <yangguanglg@163.com>
2018-10-19 15:33:27 +0800

    新闻列表前端实现、展示数据

--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,10 +1,12 @@
-var currentCid = 0; // 当前分类 id
+var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
 var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
+    //页面加载完后加载新闻数据
+    updateNewsData()
     // 首页分类切换
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
@@ -46,5 +48,35 @@ $(function () {
 })
 
 function updateNewsData() {
-    // TODO 更新新闻数据
+    // 更新新闻数据
+    var params={
+        "cid":currentCid,
+        "page":cur_page
+    }
+    $.get('/news_list',params,function(response){
+        if (response.errno=="0"){
+        //    请求成功:1、清除html已有数据(js操作)
+            $(".list_con").html("")
+        //    2、添加请求返回的数据
+             for (var i=0;i<response.data.news_dict_list.length;i++) {
+                var news = response.data.news_dict_list[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+                $(".list_con").append(content)
+            }
+
+
+        }else{
+        //    请求失败
+            alert(response.errmsg)
+
+        }
+    })
 }
