5ce1a7d0f2ab963469d3531379a35a8a5ed54a6e
Amandine95 <yangguanglg@163.com>
2018-10-20 21:06:33 +0800

    显示评论前后端

--- info/moudles/news/views.py
+++ info/moudles/news/views.py
@@ -66,12 +66,23 @@ def news_detail(news_id):
         if news in user.collection_news:
             is_collected = True
 
+    # 查询评论数据(创建时间由近及远)。
+    comments = None
+    try:
+        # 模型列表
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+    comment_dict_list = []
+    for comment in comments:
+        comment_dict_list.append(comment.to_dict())
     # 传递data，详情页继承于base页面，base中需要用到data变量所以传入data
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_list": news_dict_list,
         "news": news.to_dict(),
-        "is_collected": is_collected
+        "is_collected": is_collected,
+        "comments": comment_dict_list
 
     }
     # print(news.to_dict())
