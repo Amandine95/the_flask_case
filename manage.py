a4549d964cb4f4bce1137010007e36f2ba0be32c
Amandine95 <yangguanglg@163.com>
2018-10-23 21:08:23 +0800

    创建管理员函数集成到命令行

--- manage.py
+++ manage.py
@@ -12,6 +12,8 @@ from flask_migrate import Migrate, MigrateCommand
 from info import create_app, db, models
 
 # 通过配置名创建app
+from info.models import User
+
 app = create_app('develop')
 
 # manage文件只关注一下代码
@@ -20,6 +22,34 @@ manager = Manager(app)
 Migrate(app, db)
 # 添加迁移命令
 manager.add_command('db', MigrateCommand)
+
+
+# 创建管理员(实现命令操作创建管理员)
+# 创建命令行的装饰器,将函数添加到命令行
+# 命令行运行方式(命令窗口)：python manage.py createsuperuser -n xxx -p xxx
+# dest--> 目标 option--> 参数
+@manager.option('-n', '-name', dest='name')
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+    """创建管理员"""
+    if not all([name, password]):
+        print('参数错误')
+
+    # 初始化用户模型
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    # 是否是管理员的标志
+    user.is_admin = True
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+    print('ok')
+
 
 if __name__ == '__main__':
     manager.run()
