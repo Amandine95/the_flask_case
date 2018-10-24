c135a9c73d65f8322b1275804c82c86299cdd627
Amandine95 <yangguanglg@163.com>
2018-10-24 20:14:33 +0800

    用户统计前后端

--- info/moudles/admin/views.py
+++ info/moudles/admin/views.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime
+
 from flask import render_template, request, current_app, session, redirect, url_for, g
 
 from info.models import User
@@ -54,3 +57,41 @@ def admin_login():
         session["is_admin"] = user.is_admin
         #  跳转主界面(前缀.函数名)
         return redirect(url_for('admin.admin_index'))
+
+
+# 用户统计
+@admin_blu.route('/user_count')
+def user_count():
+    """用户统计"""
+    # 总人数
+    total_count = 0
+    try:
+        total_count = User.query.filter(User.is_admin == False).count()
+    except Exception as e:
+        current_app.logger.error(e)
+    # 月活跃(从本月1号0:0起)
+    mon_count = 0
+    # 当前时间(当年tm_year、当月tm_mon、当日tm_day、时、分、秒)
+    t = time.localtime()
+    # 月统计起始时间,返回一个时间对象
+    mon_begin_time = datetime.strptime('%d-%02d-01' % (t.tm_year, t.tm_mon), '%Y-%m-%d')
+    try:
+        # 创建时间大于本月初
+        mon_count = User.query.filter(User.is_admin == False, User.create_time > mon_begin_time).count()
+    except Exception as e:
+        current_app.logger.error(e)
+    # 日活跃(从当天0点起)
+    day_count = 0
+    day_begin_time = datetime.strptime('%d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday), '%Y-%m-%d')
+    try:
+        mon_count = User.query.filter(User.is_admin == False, User.create_time > day_begin_time).count()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    data = {
+        "total_count": total_count,
+        "mon_count": mon_count,
+        "day_count": day_count
+    }
+
+    return render_template('admin/user_count.html', data=data)
